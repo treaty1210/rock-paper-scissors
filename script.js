5e4207e272a5b938b0f74127640257709fdfb6b4
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-12 16:54:42 -0700

    Add some UI features for rps game
    Add buttons that input player choice into the game.
    Remove game loop.
    Buttons should call round function and use playerSelection of the button
    of their choice.
    Add new divs and other html elements for new features.
    Keep track of score and announce winner for first to reach 5 wins.

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//Query selectors
+const buttons = document.querySelectorAll("buttons");
+const rock = document.querySelector(".rock-button");
+const paper = document.querySelector(".paper-button");
+const scissors = document.querySelector(".scissors-button");
+const resultsDiv = document.querySelector(".results");
+const tally = document.querySelector(".score");
+const playerScoreSpan = document.querySelector(".player-score");
+const compScoreSpan = document.querySelector(".comp-score");
+
 //Determines randomly computer choice
 function getComputerChoice() {
     if (Math.floor(Math.random() * 3) === 0) {
@@ -9,56 +19,89 @@ function getComputerChoice() {
     }
 }
 
-//Main function of RPS
-function round(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return draw;
-    } else if (playerSelection === "rock"  && computerSelection === "scissors" ) {
-        return win;  
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return win;  
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return win;  
-    } else {
-        return lose;
-    }
-}
-
 //Messages for round outcome
 let playerScore = 0;
 let compScore = 0;
-let drawScore = 0;
 let draw = "It's a draw.";
 let win = "You win!";
 let lose = "You lose!";
 
-
-
-//Game loop
-function game() {
-        for (let i = 0; i < 5; i++) {
-            let playerSelection = prompt("Type in rock, paper, or scissors").toLowerCase();
-            const computerSelection = getComputerChoice(); 
-            let result = round(playerSelection, computerSelection);
-            console.log(result);
-            score(result);
-            console.log("Total Score (Player): " + playerScore);
-            console.log("Total Score (Computer): " + compScore);
-            console.log("Total Draws: " + drawScore);
-        } 
-}
-
-
-//Scoring function
-function score(result) {
-    if (result === win) {
+//Main function of RPS
+function round(playerSelection, computerSelection) {
+    console.log(playerSelection, computerSelection);
+    if (playerSelection === computerSelection) {
+        const p = document.createElement("p");
+            p.innerText = draw;
+        resultsDiv.appendChild(p);
+    } else if (playerSelection === "rock"  && computerSelection === "scissors" ) {
+        const p = document.createElement("p");
+            p.innerText = win;
+        resultsDiv.appendChild(p);
         playerScore++;
-    } else if (result === lose) {
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        const p = document.createElement("p");
+            p.innerText = win;
+        resultsDiv.appendChild(p); 
+        playerScore++;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        const p = document.createElement("p");
+            p.innerText = win; 
+        resultsDiv.appendChild(p);
+        playerScore++;
+    } else {
+        const p = document.createElement("p");
+            p.innerText = lose;
+        resultsDiv.appendChild(p);
         compScore++;
-    } else  {
-        drawScore++;
     }
 }
 
+function stopWin(playerScore, compScore) {
+    console.log("1", playerScore, "2", compScore)
+    if (playerScore === 5) {
+        const p = document.createElement("p");
+        p.style.backgroundColor = "green";
+        p.style.fontWeight = "bold";
+        p.style.color = "white";
+        p.style.fontSize = "32px";
+        p.innerText = `Player wins: ${playerScore} to ${compScore}`;
+        tally.appendChild(p);
+    } else if (compScore === 5) {
+        const p = document.createElement("p");
+        p.style.backgroundColor = "red";
+        p.style.fontWeight = "bold";
+        p.style.color = "white";
+        p.style.fontSize = "32px";
+        p.innerText = `Computer wins: ${compScore} to ${playerScore}`;
+        tally.appendChild(p);
+    }
+}
 
+//Event listeners for buttons
 
+rock.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = "rock";
+    round(playerSelection, computerSelection);
+    playerScoreSpan.innerText = `Player: ${playerScore}`;
+    compScoreSpan.innerText = `Computer: ${compScore}`;
+    stopWin(playerScore, compScore);
+});
+ 
+paper.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = "paper";
+    round(playerSelection, computerSelection);
+    playerScoreSpan.innerText = `Player: ${playerScore}`;
+    compScoreSpan.innerText = `Computer: ${compScore}`;
+    stopWin(playerScore, compScore)
+});
+
+scissors.addEventListener("click", () => {
+    const computerSelection = getComputerChoice();
+    const playerSelection = "scissors";
+    round(playerSelection, computerSelection);
+    playerScoreSpan.innerText = `Player: ${playerScore}`;
+    compScoreSpan.innerText = `Computer: ${compScore}`;
+    stopWin(playerScore, compScore)
+});
