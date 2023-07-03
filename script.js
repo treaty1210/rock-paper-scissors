4e819873af8dc8417ef8a30a6555983520799161
treaty1210 <135383414+treaty1210@users.noreply.github.com>
2023-07-03 15:14:21 -0700

    Added function to play new version
    Pressing on the images, lets user play in console currently.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //Query selectors
-const buttons = document.querySelectorAll("buttons"); //update these to be click on the images instead of buttons
-const rock = document.querySelector(".rock-button"); //also add spock and lizard
-const paper = document.querySelector(".paper-button");
-const scissors = document.querySelector(".scissors-button");
-const resultsDiv = document.querySelector(".results");
-const tally = document.querySelector(".score");
-const playerScoreSpan = document.querySelector(".player-score");
-const compScoreSpan = document.querySelector(".comp-score");
+//update these to be click on the images instead of buttons
+//also add spock and lizard
+const btns = document.querySelectorAll(".btn")
+btns.forEach(button => button.addEventListener("click", (e) => {
+    let playerSelection = String(e.target.className)
+    let computerSelection = getComputerChoice();
+    console.log(playerSelection, computerSelection);
+    round(playerSelection, computerSelection)
+}))
 
 //Determines randomly computer choice
 function getComputerChoice() {   
@@ -25,42 +26,38 @@ function getComputerChoice() {
 
 //Messages for round outcome
 let playerScore = 0; //change this to only player score and have it update in html like before (can also try to make it remain after refresh of page)
-let compScore = 0; //remove this
-let draw = "It's a draw."; //move this to html display none 
-let win = "You win!"; //move this to html display none 
-let lose = "You lose!"; //move this to html display none 
+// let draw = "It's a draw."; //move this to html display none 
+// let win = "You win!"; //move this to html display none 
+// let lose = "You lose!"; //move this to html display none 
 
 //Main function of RPS
 function round(playerSelection, computerSelection) {
-    console.log(playerSelection, computerSelection);
-    if (playerSelection === computerSelection) {
-        const p = document.createElement("p");
-            p.innerText = draw;
-        resultsDiv.appendChild(p);
-    } else if (playerSelection === "rock"  && computerSelection === "scissors" ) {
-        const p = document.createElement("p");
-            p.innerText = win;
-        resultsDiv.appendChild(p);
-        playerScore++;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        const p = document.createElement("p");
-            p.innerText = win;
-        resultsDiv.appendChild(p); 
-        playerScore++;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        const p = document.createElement("p");
-            p.innerText = win; 
-        resultsDiv.appendChild(p);
-        playerScore++;
+    if (playerSelection == computerSelection) {
+        console.log("tie")
+    } else if (playerSelection == "rock" && computerSelection == "scissors" || 
+        playerSelection == "rock" && computerSelection == "lizard") {
+        console.log("win")
+        // playerScore++;
+    } else if (playerSelection == "paper" && computerSelection == "rock" || 
+        playerSelection == "paper" && computerSelection == "spock") {
+        console.log("win")
+        // playerScore++;
+    } else if (playerSelection == "scissors" && computerSelection == "paper" ||
+        playerSelection == "scissors" && computerSelection == "lizard") {
+        console.log("win")
+        // playerScore++;
+    } else if (playerSelection == "lizard" && computerSelection == "paper" ||
+        playerSelection == "lizard" && computerSelection == "spock") {
+            console.log("win")
+    } else if (playerSelection == "spock" && computerSelection == "scissors" || 
+        playerSelection == "spock" && computerSelection == "rock") {
+            console.log("win")
     } else {
-        const p = document.createElement("p");
-            p.innerText = lose;
-        resultsDiv.appendChild(p);
-        compScore++;
+        console.log("lose")
     }
 }
 
-//NO LONGER NEED A STOP WIN FUNCTION
+//NO LONGER NEED A STOP WIN FUNCTION (OUTDATED DOES NOT WORK WITH ROCK PAPER SCISSORS LIZARD SPOCK)
 
 // function stopWin(playerScore, compScore) { 
 //     console.log("1", playerScore, "2", compScore)
@@ -84,35 +81,39 @@ function round(playerSelection, computerSelection) {
 // }
 
 //Event listeners for buttons (need to change to image instead of button, logic should still remain the same)
-//probably just remove the compscore
+//probably just remove the comp score
 //make your pick first and then computer should make a pick
 //so user select image and then the computer's choice should appear afterwards
 //after selection, selection pentagon should disappear
 //add a play again button which should reset everything and make the pentagon reappear and selection should be hidden again
 
-rock.addEventListener("click", () => { 
-    const computerSelection = getComputerChoice();
-    const playerSelection = "rock";
-    round(playerSelection, computerSelection);
-    playerScoreSpan.innerText = `Player: ${playerScore}`;
-    compScoreSpan.innerText = `Computer: ${compScore}`;
-    stopWin(playerScore, compScore);
-});
- 
-paper.addEventListener("click", () => {
-    const computerSelection = getComputerChoice();
-    const playerSelection = "paper";
-    round(playerSelection, computerSelection);
-    playerScoreSpan.innerText = `Player: ${playerScore}`;
-    compScoreSpan.innerText = `Computer: ${compScore}`;
-    stopWin(playerScore, compScore)
-});
 
-scissors.addEventListener("click", () => {
-    const computerSelection = getComputerChoice();
-    const playerSelection = "scissors";
-    round(playerSelection, computerSelection);
-    playerScoreSpan.innerText = `Player: ${playerScore}`;
-    compScoreSpan.innerText = `Computer: ${compScore}`;
-    stopWin(playerScore, compScore)
-});
+//OUTDATED EVENT LISTENERS
+// rock.addEventListener("click", () => { 
+//     const computerSelection = getComputerChoice();
+//     const playerSelection = "rock";
+//     round(playerSelection, computerSelection);
+//     playerScoreSpan.innerText = `Player: ${playerScore}`;
+//     compScoreSpan.innerText = `Computer: ${compScore}`;
+//     stopWin(playerScore, compScore);
+// });
+ 
+// paper.addEventListener("click", () => {
+//     const computerSelection = getComputerChoice();
+//     const playerSelection = "paper";
+//     round(playerSelection, computerSelection);
+//     playerScoreSpan.innerText = `Player: ${playerScore}`;
+//     compScoreSpan.innerText = `Computer: ${compScore}`;
+//     stopWin(playerScore, compScore)
+// });
+
+// scissors.addEventListener("click", () => {
+//     const computerSelection = getComputerChoice();
+//     const playerSelection = "scissors";
+//     round(playerSelection, computerSelection);
+//     playerScoreSpan.innerText = `Player: ${playerScore}`;
+//     compScoreSpan.innerText = `Computer: ${compScore}`;
+//     stopWin(playerScore, compScore)
+// });
+
+
